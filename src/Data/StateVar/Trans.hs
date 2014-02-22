d1301c80ab52d3fd080c4943b5ec9818b6b4d0e4
seagull.kamome@gmail.com <seagull.kamome@gmail.com>
2014-02-22 14:27:46 +0900

    add INLINE pragmas

--- src/Data/StateVar/Trans.hs
+++ src/Data/StateVar/Trans.hs
@@ -24,13 +24,17 @@ module Data.StateVar.Trans (
    StateVar, makeStateVar,
    -- * Utility Functions
    ($~), ($=!), ($~!),
-   (&), (@=), (@~), (@=!), (@~!), (@.)
+   (&),
+   (^=), (^~), (^=!), (^~!), (^.),
+   (@=)
 ) where
 
 import Data.IORef (IORef, readIORef, writeIORef)
 import GHC.Conc (STM, TVar, readTVar, writeTVar)
 import Data.STRef (STRef, readSTRef, writeSTRef)
 import Control.Monad.ST.Safe (ST)
+import Control.Monad.Trans (MonadTrans(..))
+import Control.Monad.Reader.Class (MonadReader(..))
 
 --------------------------------------------------------------------------------
 
@@ -53,10 +57,12 @@ newtype GettableStateVar m a = GettableStateVar (m a)
 
 instance HasGetter (GettableStateVar m) m where
    get (GettableStateVar g) = g
+   {-# INLINE get #-}
 
 -- | Construct a 'GettableStateVar' from an IO action.
 makeGettableStateVar :: m a -> GettableStateVar m a
 makeGettableStateVar = GettableStateVar
+{-# INLINE makeGettableStateVar #-}
 
 --------------------------------------------------------------------------------
 
@@ -65,20 +71,28 @@ class HasSetter s m where
    -- | Write a new value into a state variable.
    ($=) :: s a -> a -> m ()
 
-instance HasSetter IORef IO where ($=) = writeIORef
-instance HasSetter TVar STM where ($=) = writeTVar
-instance HasSetter (STRef s) (ST s) where ($=) = writeSTRef
+instance HasSetter IORef IO where
+  ($=) = writeIORef
+  {-# INLINE ($=) #-}
+instance HasSetter TVar STM where
+  ($=) = writeTVar
+  {-# INLINE ($=) #-}
+instance HasSetter (STRef s) (ST s) where
+  ($=) = writeSTRef
+  {-# INLINE ($=) #-}
 
 -- | A concrete implementation of a write-only state variable, carrying an IO
 -- action to write the new value.
 newtype SettableStateVar m a = SettableStateVar (a -> m ())
 
 instance HasSetter (SettableStateVar m) m where
-   ($=) (SettableStateVar s) a = s a
+  ($=) (SettableStateVar s) a = s a
+  {-# INLINE ($=) #-}
 
 -- | Construct a 'SettableStateVar' from an IO action.
 makeSettableStateVar :: (a -> m ()) -> SettableStateVar m a
 makeSettableStateVar = SettableStateVar
+{-# INLINE makeSettableStateVar #-}
 
 --------------------------------------------------------------------------------
 
@@ -86,18 +100,20 @@ makeSettableStateVar = SettableStateVar
 -- carrying one IO action to read the value and another IO action to write the
 -- new value.
 data StateVar m a =
-   StateVar (GettableStateVar m a) (SettableStateVar m a)
+  StateVar (GettableStateVar m a) (SettableStateVar m a)
 
 instance HasGetter (StateVar m) m where
-   get (StateVar g _) = get g
-
+  get (StateVar g _) = get g
+  {-# INLINE get #-}
 instance HasSetter (StateVar m) m where
-   ($=) (StateVar _ s) a = s $= a
+  ($=) (StateVar _ s) a = s $= a
+  {-# INLINE ($=) #-}
 
 -- | Construct a 'StateVar' from two IO actions, one for reading and one for
 -- writing.
 makeStateVar :: m a -> (a -> m ()) -> StateVar m a
 makeStateVar g s = StateVar (makeGettableStateVar g) (makeSettableStateVar s)
+{-# INLINE makeStateVar #-}
 
 --------------------------------------------------------------------------------
 
@@ -105,41 +121,52 @@ makeStateVar g s = StateVar (makeGettableStateVar g) (makeSettableStateVar s)
 -- variable with a given funtion.
 
 ($~) :: (Monad m, HasGetter v m, HasSetter v m) => v a -> (a -> a) -> m ()
-v $~ f = do
-  x <- get v
-  v $= f x
+v $~ f = get v >>= ($=) v . f
+{-# INLINE ($~) #-}
 
 -- | A variant of '$=' which is strict in the value to be set.
 ($=!) :: (Monad m, HasSetter s m) => s a -> a -> m ()
 v $=! x = x `seq` v $= x
+{-# INLINE ($=!) #-}
 
 -- | A variant of '$~' which is strict in the transformed value.
 ($~!) :: (Monad m, HasGetter v m, HasSetter v m) => v a -> (a -> a) -> m ()
-v $~! f = do
-   x <- get v
-   v $=! f x
+v $~! f = get v >>= ($=!) v . f
+{-# INLINE ($~!) #-}
 
 --------------------------------------------------------------------------------
 
 (&) :: s -> (s -> t) -> t
 s & t = t s
+{-# INLINE (&) #-}
 
-infixl 8 @=, @~, @=!, @~!, @.
+infixl 8 ^=, ^~, ^=!, ^~!, ^.
 
-(@=) :: HasSetter g m => (s -> g a) -> a -> s -> m ()
-(fv @= v) s = fv s $= v
+(^=) :: HasSetter g m => (s -> g a) -> a -> s -> m ()
+(fv ^= v) s = fv s $= v
+{-# INLINE (^=) #-}
 
-(@~) :: (Monad m, HasGetter g m, HasSetter g m) => (s -> g a) -> (a -> a) -> s -> m ()
-(fv @~ f) s = v $~ f where v = fv s
+(^~) :: (Monad m, HasGetter g m, HasSetter g m) => (s -> g a) -> (a -> a) -> s -> m ()
+(fv ^~ f) s = v $~ f where v = fv s
+{-# INLINE (^~) #-}
 
-(@=!) :: (Monad m, HasSetter g m) => (s -> g a) -> a -> s -> m ()
-(fv @=! x) s = v $=! x where v = fv s
+(^=!) :: (Monad m, HasSetter g m) => (s -> g a) -> a -> s -> m ()
+(fv ^=! x) s = v $=! x where v = fv s
+{-# INLINE (^=!) #-}
 
-(@~!) :: (Monad m, HasGetter g m, HasSetter g m) => (s -> g a) -> (a ->a) -> s -> m ()
-(fv @~! f) s = v $~! f where v = fv s
+(^~!) :: (Monad m, HasGetter g m, HasSetter g m) => (s -> g a) -> (a ->a) -> s -> m ()
+(fv ^~! f) s = v $~! f where v = fv s
+{-# INLINE (^~!) #-}
 
-(@.) :: (Monad m, HasGetter g m, HasGetter h m) => (s -> g a) -> (a -> h b) -> s -> GettableStateVar m b
-(fg @. fh) s = makeGettableStateVar $ get (fg s) >>= get . fh
+(^.) :: (Monad m, HasGetter g m, HasGetter h m) => (s -> g a) -> (a -> h b) -> s -> GettableStateVar m b
+(fg ^. fh) s = makeGettableStateVar $ get (fg s) >>= get . fh
+{-# INLINE (^.) #-}
 
 --------------------------------------------------------------------------------
+
+(@=) :: (Monad m, MonadTrans n, MonadReader s (n m), HasSetter g m) => (s -> g a) -> a -> n m ()
+fv @= v = ask >>= lift . (fv ^= v)
+{-# INLINE (@=) #-}
+
+
 
